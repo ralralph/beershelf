a46d3fd094e7c3a109688ba58e2bdb60d027acdd
setsuotokohorikawa <ralralph@gmail.com>
2019-10-23 19:12:46 +0900

    fixed:リレーションとテーブル修正

--- app/models/beer_category.rb
+++
@@ -1,4 +0,0 @@
-class BeerCategory < ApplicationRecord
-  belongs_to :beer
-  belongs_to :category
-end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,3 @@
 class Category < ApplicationRecord
-  has_many :beer_categories
-  has_many :beer_names, through: :beer_categories, source: :beer
+  has_many :beers
 end
