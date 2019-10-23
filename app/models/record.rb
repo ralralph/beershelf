1125f1da0602cfe1b269ebfc680b9cbd07fe32e2
setsuotokohorikawa <ralralph@gmail.com>
2019-10-23 13:56:57 +0900

    fixed:recordテーブルのアソシエーション修正

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class Record < ApplicationRecord
   # validates :feeling, :serving_style, presence: true
-  belongs_to :tast
+  has_one :tast
 
   enum feeling: %i[good ok bad]
   enum serving_style: %i[draft bottle can taster other]
