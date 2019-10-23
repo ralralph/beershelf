340bcff8da5d09625887fbf2851077bc93081826
setsuotokohorikawa <ralralph@gmail.com>
2019-10-23 14:11:44 +0900

    add:recordのseedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,30 @@ User.create!(
   [
     {
       id: '100',
+      name: 'A太郎',
       email: 'test1@test.com',
       password: 'password',
-      name: 'A太郎',
     },
     {
       id: '101',
+      name: 'B太郎',
       email: 'test2@test.com',
       password: 'password',
-      name: 'B太郎',
     }
   ]
 )
 
+
+Record.create!(
+  id: '100',
+  feeling: 0,
+  serving_style: 0,
+  location: nil,
+)
+
 Tast.create!(
   id: '100',
+  record_id: '100',
   bitterness: 3,
   sweetness: 3,
   sourness: 3,
