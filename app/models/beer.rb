5df70e238e39bdce3770524813a65a797b9b946d
setsuotokohorikawa <ralralph@gmail.com>
2019-10-24 12:28:29 +0900

    add:seedデータ fixed:beer category breweryのリレーション関係修正

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,3 @@
 class Beer < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :beers
+  has_many :beers, dependent: :destroy
 end
