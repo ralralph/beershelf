896952cc9d32e14baf7f20ca8c06c788fcc129a5
setsuotokohorikawa <ralralph@gmail.com>
2019-10-24 15:24:37 +0900

    fixed:syntax errorスコープメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,34 +7,34 @@ class User < ApplicationRecord
   # validates :mail, presence: true, length: { maximum: 255 },
   #           format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   # before_validation { mail.downcase! }
-has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
-has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
-has_many :followers, through: :passive_relationships, source: :follower
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
-has_many :wishlists
-has_many :beer_lists, through: :wishlists, source: :beer
+  has_many :wishlists
+  has_many :beer_lists, through: :wishlists, source: :beer
 
-# アソシエーションメソッドwishlistsからcompleteカラムにfalseが入っているデータを取り出し、beer_idのみを抽出するスコープメソッドwishingsを定義
-scope :wishings -> { wishlists.where(complete: false).select(:beer_id) }
-# アソシエーションメソッドwishlistsからcompleteカラムにtrueが入っているデータを取り出し、beer_idのみを抽出するスコープメソッドcompleatsを定義
-scope :compleats -> { wishlists.where(complete: true).select(:beer_id) }
+  # アソシエーションメソッドwishlistsからcompleteカラムにfalseが入っているデータを取り出し、beer_idのみを抽出するスコープメソッドwishingsを定義
+  scope :wishings, -> { wishlists.where(complete: false).select(:beer_id) }
+  # アソシエーションメソッドwishlistsからcompleteカラムにtrueが入っているデータを取り出し、beer_idのみを抽出するスコープメソッドcompleatsを定義
+  scope :compleats, -> { wishlists.where(complete: true).select(:beer_id) }
 
-# アソシエーションメソッドbeer_listsから、wishingsに一致するidを抽出
-scope :wishing_lists -> { beer_lists.where(id: :wishings) }
-# アソシエーションメソッドbeer_listsから、compleatsに一致するidを抽出
-scope :compleat_lists -> { beer_lists.where(id: :compleats) }
+  # アソシエーションメソッドbeer_listsから、wishingsに一致するidを抽出
+  scope :wishing_lists, -> { beer_lists.where(id: :wishings) }
+  # アソシエーションメソッドbeer_listsから、compleatsに一致するidを抽出
+  scope :compleat_lists, -> { beer_lists.where(id: :compleats) }
 
 
 
-def follow!(other_user)
-  active_relationships.create!(followed_id: other_user.id)
-end
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
 
-def following?(other_user)
-  active_relationships.find_by(followed_id: other_user.id)
-end
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
 
-def unfollow!(other_user)
-  active_relationships.find_by(followed_id: other_user.id).destroy
-end
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
