30dda2a951513764f6ec7bea011b316691697599
setsuotokohorikawa <ralralph@gmail.com>
2019-10-29 16:25:44 +0900

    edit:userのedit,updateができるよう編集。seeds追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def index
+    @users = User.all
   end
 
   def show
+    @posts = current_user.posts.select(:id, :photo).order(update: :desc) if current_user.posts.exists?
+    # @wishinglists = current_user.wishing_lists
+    # @conmpleatlist = current_user.complete_lists
   end
 
-  def edit
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
-  def update
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :posts
 
   has_many :wishlists
   has_many :beer_lists, through: :wishlists, source: :beer
@@ -25,8 +26,7 @@ class User < ApplicationRecord
   # アソシエーションメソッドbeer_listsから、compleatsに一致するidを抽出
   scope :compleat_lists, -> { beer_lists.where(id: :compleats) }
 
-
-
+  # follow機能関連のメソッド
   def follow!(other_user)
     active_relationships.create!(followed_id: other_user.id)
   end
